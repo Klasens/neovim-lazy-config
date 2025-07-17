66434ffa17332a8769e26b301d048a9ae4e9e4d4
Klasens <robfklasen@gmail.com>
2025-07-16 21:16:18 -0400

    Update keymaps and add colorscheme

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,8 +7,9 @@ vim.g.mapleader = " "
 -- Toggle Nvim-Tree (plugin will supply the command later)
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { silent = true })
 
--- Copy to system clipboard in visual mode
+-- Copy Mappings 
 vim.keymap.set("v", "<C-c>", '"+y', { silent = true })
+vim.keymap.set("n", "<C-l>", "ggVG\"+y")
 
 -- Clipboard integration for WSL users
 vim.g.clipboard = {
@@ -21,3 +22,9 @@ vim.g.clipboard = {
   cache_enabled = 0,
 }
 
+-- Scrolling and window management
+vim.keymap.set('n', '<C-u>', "<C-u>zz")
+vim.keymap.set('n', '<C-f>', "<C-f>zz")
+vim.keymap.set('n', '<leader>w', ':q<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>i', '<C-w>w', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>v", ":vsp<CR><C-w>l")
---
+++ lua/plugins/neofusion.lua
@@ -0,0 +1,49 @@
+-- lua/plugins/neofusion.lua
+-- Neofusion colorscheme configuration for lazy.nvim
+
+return {
+  "diegoulloao/neofusion.nvim",
+
+  -- load on start‑up and before anything else that might depend on colours
+  lazy = false,
+  priority = 1000,
+
+  ---@type neofusion.Config
+  opts = {
+    terminal_colors = true,
+    undercurl       = true,
+    underline       = true,
+    bold            = true,
+    italic = {
+      strings   = true,
+      emphasis  = true,
+      comments  = true,
+      operators = false,
+      folds     = true,
+    },
+    strikethrough        = true,
+    invert_selection     = false,
+    invert_signs         = false,
+    invert_tabline       = false,
+    invert_intend_guides = false,
+    inverse              = true,  -- invert background for search, diffs, etc.
+    palette_overrides    = {},    -- customise individual colours here
+    overrides = {
+      Comment              = { fg = "#8ec07c", italic = true },
+      ["@comment"]         = { fg = "#8ec07c", italic = true },
+      ["@lsp.type.comment"] = { fg = "#8ec07c", italic = true },
+    },    
+    dim_inactive         = false,
+    transparent_mode     = true,
+  },
+
+  -- lazy.nvim passes `opts` as first argument
+  config = function(_, opts)
+    -- `setup()` MUST be called *before* setting the colourscheme
+    require("neofusion").setup(opts)
+
+    vim.o.background = "dark"        -- or "light" if you prefer
+    vim.cmd.colorscheme("neofusion")
+  end,
+}
+
